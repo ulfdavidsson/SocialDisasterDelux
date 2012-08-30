aec0f2d5e0708134fd2142197f72a7b841ebf558
Ulf Davidsson <ulf.davidsson@gmail.com>
2012-08-30 10:25:29 +0200

    added better looking login buttons

--- js/views/menu/menu.js
+++ js/views/menu/menu.js
@@ -3,13 +3,46 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'text!templates/menu/menu.html'
-], function($, _, Backbone, menuTemplate){
+  'text!templates/menu/menu.html',
+  'facebookWrapper',
+], function($, _, Backbone, menuTemplate,facebookWrapper){
 
   var menuView = Backbone.View.extend({
     el: $("#navmenu"),
+	events: {	
+		'click #loginAction' : 'login',
+		'click #logoutAction' : 'logout',
+    },
+	initialize: function(){
+	
+    },  
+	login: function(){
+		facebookWrapper.run(function(FB){
+			FB.login();
+		});
+		 
+	},
+	logout: function(){
+			facebookWrapper.run(function(FB){
+			FB.logout();
+		});
+	},
     render: function(){
-      this.el.html(menuTemplate);
+		this.el.html(menuTemplate);
+		facebookWrapper.run(function(FB){
+			FB.Event.subscribe('auth.authResponseChange', function(response) {
+				if(response.status == 'connected')
+				{ 
+					$("#loginAction").hide();
+					$("#logoutAction").show();
+				}
+				else
+				{
+					$("#loginAction").show();
+					$("#logoutAction").hide();
+				}
+			});
+		}); 
     }
   });
   return new menuView;
